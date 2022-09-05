113e73763c4c34ea9d7b6509ffa6c9e587785a7e
BLACKTOOLS01 <87811689+BLACKTOOLS01@users.noreply.github.com>
2022-09-05 15:35:53 +0200

    tyfjy-teyu-r565-y5et-td-f

--- commands/general/help.js
+++ commands/general/help.js
@@ -7,7 +7,7 @@ module.exports = {
     execute(message, args) { 
        const embed = new MessageEmbed()
         .setTitle('Hent bot commands.')
-        .setDescription("```-mines get random prediction.\n\n-info show bot info.\n\n-randomnum get random number.\n\n-version get bot version.\n\n-status get bot status.```")
+        .setDescription("```-mines mines prediction.\n\n-info show bot info.\n\n-randomnum get random number.\n\n-version get bot version.\n\n-status get bot status.\n\n-crash crash prediction.\n\n-roulette roulette prediction\n\n-towers (beta).```")
         .setColor("#ff00ff")
         .setFooter("Hent!Bot commands")
        message.channel.send({ embeds: [embed] });
