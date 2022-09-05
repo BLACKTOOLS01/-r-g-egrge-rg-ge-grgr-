ac85f1cfd93cea34394071ce5f1c6f117daf0f18
BLACKTOOLS01 <87811689+BLACKTOOLS01@users.noreply.github.com>
2022-09-05 15:24:49 +0200

    rgd-e-e-gd-gd-eg-d-g-d

--- commands/predict/roulette.js
+++ commands/predict/roulette.js
@@ -1,32 +1,28 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = ({
-    name: "crash",
+    name: "roulette",
     description: "Random prediction command",
     execute(message) {
     const embed = new MessageEmbed()
         .setTitle("Hent predictor 1.00")
         .setColor('#f82f0f')
-        .addField('Prediction (crash) :globe_with_meridians:',`${TextCrash()}` + ` ${Crash()}`, )
+        .addField('Prediction (roulette) :globe_with_meridians:',`${Roulette()}`)
         .addField('Accuracy (approximately)', `${Accuracy()}` + '%.')
         .setFooter("Hent!Bot.")
         message.channel.send({ embeds: [embed] }).then(sentEmbed => {
             sentEmbed.react('👍');
-            sentEmbed.react('💣');})
+            sentEmbed.react('👎');})
  
     }
 })
 
-function TextCrash() {
-    var rand = ['Under:','Above:'];
-    return rand[Math.floor(Math.random()*rand.length)];
+function Roulette() {
+    var random = ['Red','Purple'];
+    return random[Math.floor(Math.random()*rand.length)];
 }
-function Crash() {
-    var rating = Math.floor((Math.random() * 23) + 10) /10;
-    return rating;
-}
+
 function Accuracy() {
-    var rating = Math.floor((Math.random() * 10) + 1);
+    var rating = Math.floor((Math.random() * 3) + 1);
     return rating;
 }
- 
